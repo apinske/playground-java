ad77ba0ed8694ef0a6a0d1f207ed2d14c561da8c
Alexander Pinske <alex@pinske.eu>
2021-06-05 01:02:03 +0200

    initial mobile Xcode project

---
+++ src/main/mobile/PlaygroundMobile/PlaygroundMobile/PlaygroundMobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaygroundMobileApp.swift
+//  PlaygroundMobile
+//
+//  Created by Alexander Pinske on 05.06.21.
+//
+
+import SwiftUI
+
+@main
+struct PlaygroundMobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
